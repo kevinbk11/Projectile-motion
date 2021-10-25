f6db208cd6ead0dc0ef019002d5631a9f1107453
kevinbk11 <r91090701@gmail.com>
2021-10-26 01:44:36 +0800

    normal version done.

--- src/Main.kt
+++ src/Main.kt
@@ -1,22 +1,28 @@
 import GUI.PhysicsDrawing
+import java.awt.Color
+import java.awt.Font
 import java.awt.Graphics
+import java.awt.Polygon
 import javax.swing.JFrame
 import javax.swing.JPanel
 import kotlin.math.*
 
-class drawPanel(var x:MutableList<Double>,var y:MutableList<Double>):JPanel(){
+class drawPanel(var x:MutableList<Double>,var y:MutableList<Double>,var theta:Int):JPanel(){
     override fun paint(g:Graphics)
     {
         super.paint(g)
+        g.font=Font("TimesRoman",Font.PLAIN,18)
+        g.drawString("${theta}度時有最遠射程",x.max()!!.toInt()-20,20)
         for(i in 0..x.lastIndex-1)
         {
-            g.drawLine((x[i]).toInt(),1080-(y[i]).toInt(),(x[i+1]).toInt(),1080-(y[i+1]).toInt())
+            g.drawLine((x[i]).toInt(),(y.max()!!-y[i]).toInt(),(x[i+1]).toInt(),(y.max()!!-y[i+1]).toInt())
         }
+        g.color= Color.BLACK
     }
 }
 fun main(args:Array<String>)
 {
-    val j = JFrame("test")
+    val j = JFrame("")
     val p = PhysicsDrawing()
     var j2 = JFrame("結果")
     p.button1.addActionListener {
@@ -28,21 +34,22 @@ fun main(args:Array<String>)
         }
         var bigXL = mutableListOf<Double>()
         var bigYL = mutableListOf<Double>()
-        var ans:Int=0
+        var ans=0
         var bigX = 0.0
         for(theta in 0..90)
         {
             var yL = 0.1
-            var i =0.001
+            var i =0.000
             val x= mutableListOf<Double>()
             val y= mutableListOf<Double>()
             val vx = cos(3.14159/180*theta)*v
             val vy = sin(3.14159/180*theta)*v
-            while(yL>0)
+            while(yL>=0)
             {
-                x.add(i*vx*10)
-                yL=-4.9*i*i+vy*i+h/100.0
-                y.add(yL*(60+sqrt((h/100).toFloat())))
+                yL=-4.9*i*i+vy*i+h/100
+                if(yL<0)break
+                x.add(i*vx*50)
+                y.add(yL*50)
                 i+=0.001
             }
             if(x.last()>bigX)
@@ -58,11 +65,11 @@ fun main(args:Array<String>)
         j2 = JFrame("結果")
         with(j2)
         {
-
-            setSize(1920,1080)
+            setSize(bigXL.max()!!.toInt()+180,(bigYL.max()!!).toInt()+40)
+            setLocation(200,0)
             isVisible=true
             isResizable=false
-            add(drawPanel(bigXL,bigYL))
+            add(drawPanel(bigXL,bigYL,ans))
         }
     }
     j.setSize(200,100)
